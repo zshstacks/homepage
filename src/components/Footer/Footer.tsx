4dbe30e6de1e4fdff861c6092665a378643722a2
wlr1 <uns4d123@gmail.com>
2026-02-12 00:12:37 +0200

    FIXED: footer bug

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,17 +2,18 @@ import { memo } from "react";
 import { useTranslation } from "react-i18next";
 
 const Footer = memo(() => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <footer className="w-full">
-            <div className="flex justify-center mt-12 mb-6">
-                <p className="text-sm text-white/30 dark:text-gray-800/30">
-                    {t("footer")}
-                </p>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="w-full mt-auto">
+      {" "}
+      <div className="flex justify-center py-6">
+        <p className="text-sm text-white/30 dark:text-gray-800/30">
+          {t("footer")}
+        </p>
+      </div>
+    </footer>
+  );
 });
 
 Footer.displayName = "Footer";
